c115cc9e9de263397aa3a819e2ca95e44f4a7b65
somethingsomethingtwice <341150100@gapps.yrdsb.ca>
2024-04-30 19:24:55 -0400

    Started the project, minor documentation

---
+++ Elephant.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Our glorious saviour, the Elephant.
+ * 
+ * @author Ethan 
+ * @version April 2024
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Game World.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Ethan 
+ * @version April 2024
  */
 public class MyWorld extends World
 {
@@ -17,5 +17,8 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
     }
 }
