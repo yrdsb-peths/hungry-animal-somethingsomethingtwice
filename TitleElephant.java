d0e2dfc071b055c6342e8dbfbda2428aa6fc0a92
somethingsomethingtwice <341150100@gapps.yrdsb.ca>
2024-05-13 14:30:29 -0400

    titlescreen flourish

---
+++ TitleElephant.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleElephant here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleElephant extends Actor
+{
+    public void addedToWorld(World world) {
+        getImage().scale(200, 180);    
+    };
+    
+    /**
+     * Act - do whatever the TitleElephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- TitleScreen.java
+++ TitleScreen.java
@@ -8,8 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label label = new Label("Hungry Elephant", 60);
-    
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -18,13 +16,31 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        addObject(label, 300, 200);
+        prepare();
     }
-    
+
     public void act() {
         if (Greenfoot.isKeyDown("space")) {
             MyWorld newWorld = new MyWorld();
             Greenfoot.setWorld(newWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Label label = new Label("Hungry Elephant", 60);
+        addObject(label,264,48);
+        Label label2 = new Label("Move using Arrow Keys", 30);
+        addObject(label2,200,96);
+        Label label3 = new Label("Don't let the apple fall to the bottom!", 30);
+        addObject(label3,304,128);
+        Label label4 = new Label("Press Space to start", 30);
+        addObject(label4,416,263);
+        TitleElephant titleElephant = new TitleElephant();
+        addObject(titleElephant,120,263);
     }
 }
